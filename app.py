a9a73ec3f1d8a632bb5d95ba514871d6d6d8cc1f
sam <wax654@gmail.com>
2021-02-23 15:55:15 -0800

    added form to main page

--- app.py
+++ app.py
@@ -9,8 +9,16 @@ app.config["SECRET_KEY"] = "a secret key"
 boggle_game = Boggle()
 
 @app.route('/')
-def main_boggle_page():
-    board = boggle_game.make_board()
-    session['board'] = board
-    print(board)
+def display_boggle_game():
+    """gets the boggle game and displays it for the user"""
+    board = get_boggle_game()
     return render('boggle-board.html', board=board)
+
+def get_boggle_game():
+    """returns the current boggle game board. If none in session already, it makes a new one, stores it in the session, then returns it"""
+    if len(session['board']):
+        return session['board']
+    else:
+        board = boggle_game.make_board()
+        session['board'] = board
+        return board
--- test.py
+++ test.py
@@ -4,11 +4,24 @@ from flask import session
 from boggle import Boggle
 
 
+# Make Flask errors be real errors, not HTML pages with error info
+app.config['TESTING'] = True
+
+# This is a bit of hack, but don't use Flask DebugToolbar
+app.config['DEBUG_TB_HOSTS'] = ['dont-show-debug-toolbar']
+
+
 class FlaskTests(TestCase):
 
     # TODO -- write tests for every view function / feature!
 
     def test_initial_load(self):
-        with app.get_client() as client:
+        with app.test_client() as client:
             res = client.get('/')
             self.assertEqual(res.status_code, 200)
+            html = res.get_data(as_text=True)
+            
+            self.assertIn('<h1 class="title">BOGGLE!</h1>', html)
+            #the board in the session should have 5 arrays inside
+            self.assertEqual(len(session['board']), 5)
+            
